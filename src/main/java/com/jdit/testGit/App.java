13d4cb154f7a7983606a95f759d6da07480407ef
tom_glb <googMorning_glb@qq.com>
2020-01-10 17:22:41 +0800

    111111111

--- src/main/java/com/jdit/testGit/App.java
+++ src/main/java/com/jdit/testGit/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        System.out.println( "Hello World!22222" );
-
+       System.out.println( "Hello World!22222222" );
     }
 }
