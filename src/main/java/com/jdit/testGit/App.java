bd05889520539f3602715c6376fb14094174d42c
shanghai <atiguigu@qq.com>
2020-01-12 19:59:10 +0800

    dfds

--- src/main/java/com/jdit/testGit/App.java
+++ src/main/java/com/jdit/testGit/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!11111113332222" );
     
        System.out.println( "Hello World!22222222uuuu" );
+       
+       System.out.println("testgit host_fix");
     }
 }
